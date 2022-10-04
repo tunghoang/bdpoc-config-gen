6bf4cf4ebeb8a6e8524ea00a1c161e83b168811f
MrYasuo <buihai0706vn@gmail.com>
2022-10-04 09:54:06 +0700

    update

--- visualize/checks/check_schedule.py
+++ visualize/checks/check_schedule.py
@@ -1,13 +1,17 @@
 import schedule
 import pandas as pd
 from ..influx_utils import *
+from Query import Query
 
+client = InfluxDBClient.from_config_file("../config.ini")
 current_data = pd.DataFrame()
 
 def job():
-    print("job")
+	query = Query().from_bucket(bucket).range("1m").aggregate_window(True).pivot("_time", "_field", "_value").to_str()
+	table = client.query_api().query_data_frame(query, org=org)
+	return table
 
-schedule.every(1).second.do(job)
+schedule.every(1).minute.do(job)
 
 while True:
-    schedule.run_pending()
+	schedule.run_pending()
