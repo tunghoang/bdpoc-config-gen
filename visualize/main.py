47ec95831dc47717fb23427a868f0302b2f816ba
MrYasuo <buihai0706vn@gmail.com>
2022-10-05 23:06:47 +0700

    fix

--- visualize/main.py
+++ visualize/main.py
@@ -9,40 +9,39 @@ from configs.module_loader import *
 # from checks import check_schedule
 
 def init():
-    # Initialize
-    st.set_page_config(page_title="Home", page_icon=":house:", layout="wide")
-    st.markdown(local_css(path.join(path.dirname(__file__), "assets", "style.css")), unsafe_allow_html=True)
-    # Init session
-    init_session("tags", [])
-    init_session("search_tags", "")
-    init_session("selected_device_name", "")
-    init_session("interpolated", False)
-    init_session("time_range", 10)
-    init_session("start_time", DATE_NOW() - dt.timedelta(days=1))
-    init_session("end_time", DATE_NOW())
-    init_session("difference_time_range", cal_different_time_range())
-    init_session("chart_mode", "all")
-    init_session("table_mode", "thin")
-    init_session("pivot_state", False)
-    init_session("call_influx", False)
-    init_session("data", pd.DataFrame())
+	# Initialize
+	st.set_page_config(page_title="Home", page_icon=":house:", layout="wide")
+	st.markdown(local_css(path.join(path.dirname(__file__), "assets", "style.css")), unsafe_allow_html=True)
+	# Init session
+	init_session("tags", [])
+	init_session("search_tags", "")
+	init_session("selected_device_name", "")
+	init_session("interpolated", False)
+	init_session("time_range", 10)
+	init_session("start_time", DATE_NOW() - dt.timedelta(days=1))
+	init_session("end_time", DATE_NOW())
+	init_session("difference_time_range", cal_different_time_range())
+	init_session("chart_mode", "all")
+	init_session("table_mode", "thin")
+	init_session("pivot_state", False)
+	init_session("call_influx", False)
+	init_session("data", pd.DataFrame())
 
 def main():
-    init()
-    # Load config file
-    control_logic_checks, deviation_checks, devices = load_tag_config()
-    print(devices)
-    with st.sidebar:
-        render_sidebar(devices)
+	init()
+	# Load config file
+	control_logic_checks, deviation_checks, devices = load_tag_config()
+	with st.sidebar:
+		render_sidebar(devices)
 
-    # placeholder = st.empty()
-    # if (st.session_state["call_influx"]):
-    #     placeholder = st.empty()
-    #     load_data()
-    #     st.session_state["call_influx"] = False
-    # with placeholder.container():
-    #     render_configurations()
-    #     render_columns(devices)
+	placeholder = st.empty()
+	if (st.session_state["call_influx"]):
+		placeholder = st.empty()
+		load_data()
+		st.session_state["call_influx"] = False
+	with placeholder.container():
+		render_configurations()
+		render_columns(devices)
 
 if __name__ == "__main__":
-    main()
+	main()
--- visualize/utils/influx_utils.py
+++ visualize/utils/influx_utils.py
@@ -1,39 +1,39 @@
-from visualize.configs.module_loader import *
-from visualize.configs.Query import Query
+from configs.module_loader import *
+from configs.Query import Query
 from configs.influx_client import client
 from configs.constants import BUCKET, ORG
 
 warnings.simplefilter("ignore", MissingPivotFunction)
 
 def query_data(time: int, device: str, tags: list = [], interpolated: bool = False, missing_data: str = "NaN", table_mode: str = "thin") -> pd.DataFrame:
-    if (not device) or (len(tags) == 0):
-        return DataFrame()
-    query = Query().from_bucket(BUCKET).range(time).filter_measurement(device).filter_fields(tags).keep_columns("_time", "_value", "_field")
-    if interpolated:
-        query = query.interpolate()
-    query = query.aggregate_window(True if missing_data == "NaN" else False).to_str()
-    # print(query)
-    pivot_query = Query().from_bucket(BUCKET).range(time).filter_measurement(device).filter_fields(tags).keep_columns("_time", "_value", "_field").aggregate_window(True).pivot("_time", "_field", "_value").to_str()
-    table = None
-    if table_mode == "thin":
-        table = client.query_api().query_data_frame(query, org=ORG)
-        if not table.empty:
-            # FORMAT TABLE
-            # Add missing tags
-            no_data_tags = [tag for tag in st.session_state["tags"] if tag not in table["_field"].values]
-            missing_tags_df = pd.DataFrame({"_field": no_data_tags, "_time": [table["_time"][0] for _ in no_data_tags]})
-            table = pd.concat([missing_tags_df, table], join="outer")
-    elif table_mode == "fat":
-        table = client.query_api().query_data_frame(pivot_query, org=ORG)
-        if not table.empty:
-            # FORMAT TABLE
-            # Add missing tags
-            no_data_tags = [tag for tag in st.session_state["tags"] if tag not in table.columns]
-            has_data_tags = [tag for tag in st.session_state["tags"] if tag in table.columns]
-            for tag in no_data_tags:
-                table[tag] = table[has_data_tags[0]] = np.nan
-    else:
-        raise Exception("Unknown table mode")
-    if "_time" in table:
-        table["_time"] = table["_time"].dt.tz_convert(pytz.timezone("Asia/Ho_Chi_Minh"))
-    return table
+	if (not device) or (len(tags) == 0):
+		return DataFrame()
+	query = Query().from_bucket(BUCKET).range(time).filter_measurement(device).filter_fields(tags).keep_columns("_time", "_value", "_field")
+	if interpolated:
+		query = query.interpolate()
+	query = query.aggregate_window(True if missing_data == "NaN" else False).to_str()
+	# print(query)
+	pivot_query = Query().from_bucket(BUCKET).range(time).filter_measurement(device).filter_fields(tags).keep_columns("_time", "_value", "_field").aggregate_window(True).pivot("_time", "_field", "_value").to_str()
+	table = None
+	if table_mode == "thin":
+		table = client.query_api().query_data_frame(query, org=ORG)
+		if not table.empty:
+			# FORMAT TABLE
+			# Add missing tags
+			no_data_tags = [tag for tag in st.session_state["tags"] if tag not in table["_field"].values]
+			missing_tags_df = pd.DataFrame({"_field": no_data_tags, "_time": [table["_time"][0] for _ in no_data_tags]})
+			table = pd.concat([missing_tags_df, table], join="outer")
+	elif table_mode == "fat":
+		table = client.query_api().query_data_frame(pivot_query, org=ORG)
+		if not table.empty:
+			# FORMAT TABLE
+			# Add missing tags
+			no_data_tags = [tag for tag in st.session_state["tags"] if tag not in table.columns]
+			has_data_tags = [tag for tag in st.session_state["tags"] if tag in table.columns]
+			for tag in no_data_tags:
+				table[tag] = table[has_data_tags[0]] = np.nan
+	else:
+		raise Exception("Unknown table mode")
+	if "_time" in table:
+		table["_time"] = table["_time"].dt.tz_convert(pytz.timezone("Asia/Ho_Chi_Minh"))
+	return table
