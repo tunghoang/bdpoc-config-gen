fc50bc8705973f8a547de958c8c697c84689e84d
Tung HOANG <tung.hoang@gmail.com>
2022-10-05 19:37:39 +0700

    add device filter in check

--- visualize/checks/check_schedule.py
+++ visualize/checks/check_schedule.py
@@ -20,13 +20,13 @@ current_data = pd.DataFrame()
 
 def formater(df, measurement):
     df["_time"] = pd.to_datetime(df['_time'], errors='coerce').astype(np.int64)
-    lines = [{"measurement": f"{measurement}", "fields": {row["_field"]: float(row["_value"])}, "time": row["_time"]} for _, row in df.iterrows() if row["_value"] != 0 and not math.isnan(row["_value"])]
+    lines = [{"measurement": f"{measurement}", "tags": {"device": row["_measurement"]}, "fields": {row["_field"]: float(row["_value"])}, "time": row["_time"]} for _, row in df.iterrows() if row["_value"] != 0 and not math.isnan(row["_value"])]
     return lines
 
 PandasObject.formater = formater
 
 def job():
-    query = Query().from_bucket(bucket).range(f"{2 * CHECK_PERIOD}m").keep_columns("_time", "_value", "_field").aggregate_window(True).to_str()
+    query = Query().from_bucket(bucket).range(f"{2 * CHECK_PERIOD}m").keep_columns("_time", "_value", "_field", "_measurement").aggregate_window(True).to_str()
     table = query_api.query_data_frame(query, org=org)
     nan_checks = nan_check(table, tags).drop(columns=["_start", "_stop", "result", "table"]).formater("nan_checks")
     overange_checks = overange_check(table, tags).drop(columns=["_start", "_stop", "result", "table"]).formater("overange_checks")
@@ -36,7 +36,8 @@ def job():
     write_api.write("check-datahub", org, record=irv_checks)
     return table
 
-schedule.every(CHECK_PERIOD).minute.do(job)
+# schedule.every(CHECK_PERIOD).minute.do(job)
 
-while True:
-    schedule.run_pending()
+# while True:
+#     schedule.run_pending()
+job()
