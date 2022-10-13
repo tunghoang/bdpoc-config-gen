82a323b5f88f0ba543ff522d0624bd474a68ba2e
Tung HOANG <tung.hoang@gmail.com>
2022-10-13 15:04:06 +0700

    test check

--- visualize/configs/checks.py
+++ visualize/configs/checks.py
@@ -63,14 +63,15 @@ def irv_check(df: pd.DataFrame, devices, tags: list = [], pivot: bool = False) -
     for tag in tags:
       if tag in df.columns:
         max3, max2, max1, min1, min2, min3 = find_low_high_irv_by_devices(devices, tag)
-        res[tag] = [
-            np.nan if math.isnan(value) else 3 if value >= max3 else 2 if value >= max2 and value < max3 else 1 if value >= max1 and value < max2 else 0 if value >= min1 and value < max1 else -1 if value >= min2 and value < min1 else -2 if value >= min3 and value < min2 else -3
-            for value in res[tag].values
-        ]
+        if max3 and max2 and max1 and min1 and min2 and min3:
+          res[tag] = [
+              np.nan if math.isnan(value) else 3 if value >= max3 else 2 if value >= max2 and value < max3 else 1 if value >= max1 and value < max2 else 0 if value >= min1 and value < max1 else -1 if value >= min2 and value < min1 else -2 if value >= min3 and value < min2 else -3
+              for value in res[tag].values
+          ]
     return res
   for tag in tags:
     max3, max2, max1, min1, min2, min3 = find_low_high_irv_by_devices(devices, tag)
-    if (max3 and max2 and max1 and min1 and min2 and min3):
+    if max3 and max2 and max1 and min1 and min2 and min3:
       res["_value"] = [
           row["_value"] if row["_field"] != tag else np.nan if math.isnan(row["_value"]) else 3 if row["_value"] >= max3 else
           2 if row["_value"] >= max2 and row["_value"] < max3 else 1 if row["_value"] >= max1 and row["_value"] < max2 else 0 if row["_value"] >= min1 and row["_value"] < max1 else -1 if row["_value"] >= min2 and row["_value"] < min1 else -2 if row["_value"] >= min3 and row["_value"] < min2 else -3
