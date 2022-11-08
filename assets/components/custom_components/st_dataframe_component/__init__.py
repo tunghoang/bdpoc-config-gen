1ec00f6f0b26d590574756921145b92dfeedaaf4
Tung HOANG <tung.hoang@gmail.com>
2022-11-08 20:30:04 +0700

    update module

--- assets/components/custom_components/st_dataframe_component/__init__.py
+++ assets/components/custom_components/st_dataframe_component/__init__.py
@@ -4,7 +4,6 @@ import streamlit.components.v1 as components
 
 parent_dir = os.path.dirname(os.path.abspath(__file__))
 build_dir = os.path.join(parent_dir, "frontend/build")
-
 _component_func = components.declare_component("st_dataframe_component", path=build_dir)
 
 
