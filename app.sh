cd06f295a2584af20d15d1fd0bbd46362ff58a69
Tung HOANG <tung.hoang@gmail.com>
2022-12-09 10:10:33 +0000

    adjust constants

--- app.sh
+++ app.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script will start the server
-.datahub/bin/streamlit run visualize/app.py
+./.datahub/bin/streamlit run visualize/app.py --server.enableCORS=false --server.enableXsrfProtection=false
